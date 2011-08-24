3d76ec238277418467ef29a362ff4026bab7b936
Ewoud Kohl van Wijngaarden <e.kohlvanwijngaarden@oxilion.nl>
2011-08-24 16:47:34 +0200

    dns: Add output in bind config files

--- dns/urls.py
+++ dns/urls.py
@@ -15,4 +15,19 @@ urlpatterns = patterns('dns.views',
         ),
         name='domain_detail'
     ),
+
+    url(r'^bind/named.zones.conf$', ListView.as_view(
+            model=Domain,
+            context_object_name="domain_list",
+            template_name="dns/bind/named.zones.conf",
+        ),
+        name='bind_named.zones.conf'
+    ),
+    url(r'^bind/(?P<pk>(\w+\.)+\w+).zone$', DetailView.as_view(
+            model=Domain,
+            context_object_name="domain",
+            template_name="dns/bind/template.zone",
+        ),
+        name='bind_domain.zone'
+    ),
 )
