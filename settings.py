6de4a43ec2e9dc53995187f88b7663b7d63fd50a
Ewoud Kohl van Wijngaarden <e.kohlvanwijngaarden@oxilion.nl>
2011-08-24 12:41:05 +0200

    Set the database

--- settings.py
+++ settings.py
@@ -1,4 +1,7 @@
 # Django settings for demo project.
+import os
+
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -11,12 +14,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(SITE_ROOT, 'sqlite3.db'),
     }
 }
 
