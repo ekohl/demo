27f6680367376408dbc77f37d9e6b5151f4707a3
Ewoud Kohl van Wijngaarden <e.kohlvanwijngaarden@oxilion.nl>
2011-08-24 13:40:08 +0200

    dns: Define simple models

--- dns/models.py
+++ dns/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Domain(models.Model):
+    name = models.CharField(max_length=63, primary_key=True)
+    ttl = models.PositiveIntegerField(default=14400)
+
+class DnsRecord(models.Model):
+    RR_TYPES = (
+            ('A', 'Host Address'),
+            ('NS', 'Name Server'),
+            ('CNAME', 'Canonical Name'),
+            ('PTR', 'Domain Name Pointer'),
+            ('MX', 'Mail Exchange'),
+            ('TXT', 'Text'),
+            ('SRV', 'Service'),
+    )
+
+    domain = models.ForeignKey(Domain)
+    name = models.CharField(max_length=63)
+    type = models.CharField(max_length=5, choices=RR_TYPES)
+    ttl = models.PositiveIntegerField(blank=True)
+    data = models.CharField(max_length=255)
